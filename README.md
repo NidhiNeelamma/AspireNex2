0d8f3bdfbc0874cdb1627ef77bd5127d6d2c8be3
NidhiNeelamma <161196532+NidhiNeelamma@users.noreply.github.com>
2024-07-03 15:42:13 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# QuizApp
+I've created a platform that enables users to create and take quizzes. Users can input questions, multiple-choice 
+answers, and correct answers to build a quiz and also users can then take the quiz and receive immediate 
+feedback on their scores.
+Functions:
+ Create Quiz, Update Quiz, Take Quiz. 
+ Made using php and mysql.
+
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body style="background-size: cover;">
+    <header>
+        <div class="container">
+        <span id="time" style="color: black;"></span>
+                <p>Quiz</p>
+        </div>
+    </header>
+    <main>
+        <div class="menu">
+            <a href="question.php?n=1" class="btnadd">Start Quiz</a>
+            <a href="add.php" class="btnadd">Add Questions</a>
+        </div>
+    </main>
+    
+     <script>
+        let a;
+        let date;
+        let time;
+        const options = {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+        };
+        setInterval(() => {
+            a = new Date();
+            date = a.toLocaleDateString(undefined, options);
+            time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+            const dayOfWeek = a.toLocaleDateString(undefined, { weekday: 'long' });
+            document.getElementById('time').innerHTML = date + " - Time: " + time;
+            document.getElementById('item-list').innerHTML = itemHTML;
+        }, 1000);
+
+    </script>
+</body>
+</html>
